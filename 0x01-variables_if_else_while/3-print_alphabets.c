8846fd19b8329878ed0e8262c85536172f89aabb
nebihadilsebo <shegolenur@hotmail.com>
2022-12-10 09:20:51 -0800

    task2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - return alphabet 
+ *
+ * Return: Always zero
+ */
+int main(void)
+	int ch = 'a';
+
+	while (ch <= '2')
+{
+	putchar(ch);
+	ch++;
+}
+putchar('\n');
+return (0);
+
