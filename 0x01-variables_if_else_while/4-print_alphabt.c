69476742ad58eb309c4d8c65a30b328997c2d077
nebihadilsebo <shegolenur@hotmail.com>
2022-12-10 15:00:12 -0800

    number

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 /**
  * main - main block
  * description: get a random check and check its
