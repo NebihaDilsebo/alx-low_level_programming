fe3655d5f4f680d89d4d10a6cdaa00de39c42921
nebihadilsebo <shegolenur@hotmail.com>
2022-12-11 12:19:06 -0800

    combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 			{
-			for (ones = '0'; ones <= '9'; ones++)/*prints ones digit
+			for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
 					{
 					if (!((ones == tens) || (tens > ones)))/* eliminate repitition*/
 					{
