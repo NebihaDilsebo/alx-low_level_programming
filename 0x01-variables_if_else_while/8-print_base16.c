fe1bdf031f855bc9360c68f8b6a2717a52660ec9
nebihadilsebo <shegolenur@hotmail.com>
2022-12-11 00:55:04 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * description: prints all numbers of base 16 followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c = 'a';
+	char b = '0';
+
+	while
+		(c <= 'f')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(b <= 9)
+		{
+			putchar(b);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
