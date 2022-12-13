2916610deb92826c3b4d746c66434b7ee689ffb8
nebihadilsebo <shegolenur@hotmail.com>
2022-12-13 13:44:54 -0800

    print

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,9 +10,9 @@ void print_alphabet(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c);
+		_putchar(c);
 		{
-			_putchar)'\n');
+			_putchar('\n');
 		}
 	}
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * _islower - checks if a character is lowercase
+ * @c: the character
+ * Return: 1 if letter is lowercase, 0 ifnot
+ */
+int _islower(int c)
