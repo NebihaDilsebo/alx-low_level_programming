06c49f45550315c4daeb50bab422e294a718c936
nebihadilsebo <shegolenur@hotmail.com>
2022-12-13 11:31:30 -0800

    char

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet - entry point
- *
+ * print_alphabet_x10 - Entry point
+ * description: print 10x
  * Return: 0 (success)
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar ('\n');
+}
 
 
