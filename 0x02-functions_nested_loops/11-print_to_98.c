7af54d36148b80ece67eda571f2e45b30931e2dc
nebihadilsebo <shegolenur@hotmail.com>
2022-12-14 03:24:26 -0800

    natural no to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+ * print_to_98 - print natural number from n to 98
+ * @n: the number
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+	printf("98");
+	putchar('\n');
+}
