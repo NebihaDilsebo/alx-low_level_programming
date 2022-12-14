ad4e8f48a6f71bbc8b08edcbba78a3514141d727
nebihadilsebo <shegolenur@hotmail.com>
2022-12-14 02:26:19 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * add - add two integers and return the result
+ * @i: prints int i
+ * @k: prints int k
+ * Return: 0 (success)
+ */
+
+int add(int i, int k);
+{
+	return (i + k);
+}
+
