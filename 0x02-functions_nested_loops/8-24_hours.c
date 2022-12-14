9524d26a6ba2674d6c3b464e3f0435c18c51e499
nebihadilsebo <shegolenur@hotmail.com>
2022-12-14 00:25:01 -0800

    every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 59; a++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (b = 0; b <= 23; b++)
+		for (b = 0; b <= 59; b++)
 		{
 			_putchar (a / 10 + '0');
 			_putchar (a % 10 + '0');
