64e71f6a94425e7642d0ea7f4627ba70e15f092a
nebihadilsebo <shegolenur@hotmail.com>
2022-12-17 02:08:00 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+nclude "main.h"
+
+/**
+ * print_diagonal - print diagonal line
+ * @n: intiger para
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		
+		for (x = 0; x < n);
+		{
+			for (i = 0; i <= x; i++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
