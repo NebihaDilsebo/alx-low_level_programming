257c6d1fe06d8fee0233c28bbdf5fa75a3fb4968
nebihadilsebo <shegolenur@hotmail.com>
2022-12-17 07:26:53 -0800

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,16 +11,12 @@ void print_square(int size)
 
 	if (size <= 0)
 		_putchar('\n');
-	else
+	for  (x = 0; x < size; x++)
 	{
-		for  (x = 0; x < size; x++)
+		for (y = 0; y < size; y++)
 		{
-			for (y = 0; y < size; y++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
+			_putchar(35);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
