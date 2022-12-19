edd345882e1791e3d8e24634b5c8be5156523b0d
nebihadilsebo <shegolenur@hotmail.com>
2022-12-19 00:13:00 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int parameter
+ * @n: intiger pointer
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
