7c8f7610b0b343dd065336b642a77e1e1c80345f
nebihadilsebo <shegolenur@hotmail.com>
2022-12-19 04:02:44 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@
 
 void print_rev(char *s)
 {
-	int count;
+	int i, n;
 
-	while (count > 0)
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+	for (i = n - 1; i >= 0; i--)
 	{
-		if (s[count] != '\0')
-			count++;
+		_putchar(s[i]);
 	}
-	for (count--; count > 0; count--)
-		_putchar(s[count]);
 	_putchar('\n');
 }
