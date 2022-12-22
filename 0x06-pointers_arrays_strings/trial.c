f4f72742d9aac86e8d30c25b68544c845e98bd71
nebihadilsebo <shegolenur@hotmail.com>
2022-12-22 02:47:15 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/trial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two string
+ * @src: first parameter
+ * @dest: second parameter
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char[30] = "Hello";
+	char s2[7] = "World!";
+
+	printf("%s\n",s1);
+	return (dest);
+}
+
