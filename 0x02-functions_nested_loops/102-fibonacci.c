cb755d882acdbdc8a5a63ebab0ef5656ed308fbf
nebihadilsebo <shegolenur@hotmail.com>
2022-12-28 06:48:51 -0800

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	int n;
-        long int i, j, k;
+	long int i, j, k;
 	
 	i = 0;
 	j = 1;
 	k = i + j;
 	for (n = 0; n < 50; n++)
 	{
-		printf("%ld, \n", k);
+		printf("%ld, ", k);
 		i = j;
 		j = k;
 		k = i + j;
