cf6169c309689041908cc2c3ca5414d7e8b2019c
nebihadilsebo <shegolenur@hotmail.com>
2023-01-04 07:23:39 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *@s: string parameter
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	s = "lpa\0"
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	s++;
+}
+
+
