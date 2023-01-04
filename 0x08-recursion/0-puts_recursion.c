b26fe4d70d58d35c9e5fecd4380699ff74723eb1
nebihadilsebo <shegolenur@hotmail.com>
2023-01-04 08:24:38 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
+ * _puts_recursion - prints a string followed by new line
  *@s: string parameter
- * Return: 0
+ * Return: void
  */
 
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
-	s = "lpa\0";
 	if (*s == '\0')
 	{
 		_putchar('\0');
