07996a5fad72c7890b1e2c7a4891d1096c5e3896
nebihadilsebo <shegolenur@hotmail.com>
2023-01-11 04:07:34 -0800

    task6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,7 +9,7 @@ char  **strtow(char *str);
  * first word contained within a string
  * @str: the string to be searched
  *
- * Return: the index markingthe end of the initial word pointed to by str
+ * Return: the index marking the end of the initial word pointed to by str
  */
 
 int word_len(char *str)
@@ -23,3 +23,27 @@ int word_len(char *str)
 	}
 	return (len);
 }
+
+/**
+ * count_words - counts the number of words contained within a string
+ * @str: the string to be searched
+ * Return: the number of words contained within str
+ */
+
+int count_words(char *str)
+{
+	int index = 0, words = 0, len = 0;
+
+	for (index = 0; *(str + index); index++);
+			len++;
+
+			for (index = 0; index < len; index++)
+			{
+			if (*(str + index) != ' ')
+			{
+			words++;
+			index += word_len(str + index);
+			}
+			}
+			return (words);
+			}
