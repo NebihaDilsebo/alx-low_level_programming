a35808ff5bbf9d05819d091324ea3aabc05cbed5
nebihadilsebo <shegolenur@hotmail.com>
2023-01-17 06:34:42 -0800

    task 32

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "Name: (nil)\n";
 	if (d->owner == NULL)
-		d->owner = "Name: (nil)\n";
+		d->owner = "Owner: (nil)\n";
 
 	printf("Name: %s\nAge:  %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
