7839614b1ecc4f87335208b57d5363de979d63e7
nebihadilsebo <shegolenur@hotmail.com>
2023-01-18 12:26:38 -0800

    task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculate the sum of two intigers
+ * @a: first intiger
+ * @b: second intiger
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculates the subtraction of two intigers
+ * @a: first intiger
+ * @b: second intiger
+ *
+ * Return: difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates the multiplication ot two intigers
+ * @a: first intiger
+ * @b: second intiger
+ *
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates the division of two intigers
+ * @a: first intiger
+ * @b: second intiger
+ *
+ * Return:  the division of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the module of two number
+ * @a: first intiger
+ * @b: second intiger
+ *
+ * Return: the module of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
