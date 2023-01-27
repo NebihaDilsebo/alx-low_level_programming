76cb99c9fc1655fe96312d73ead338e69f25ba86
nebihadilsebo <shegolenur@hotmail.com>
2023-01-27 02:50:31 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,20 +11,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 0;
+	size_t s = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h->next != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[%d]%s\n", 0, "(nil)");
-
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		node_count += 1;
+		s++;
 	}
-	printf("[%d]%s\n", h->len, h->str);
-	return (node_count);
+	return (s);
 }
