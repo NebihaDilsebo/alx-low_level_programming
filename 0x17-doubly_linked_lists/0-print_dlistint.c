b1d6c68b7ddb8822397f066137a08a122d199b15
nebihadilsebo <shegolenur@hotmail.com>
2023-03-16 07:39:58 -0700

    doubly_linked_listsdoubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,6 +11,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
+
 	count = 0;
 
 	if (h == NULL)
@@ -22,7 +23,7 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count ++;
+		count++;
 		h = h->next;
 	}
 	return (count);
