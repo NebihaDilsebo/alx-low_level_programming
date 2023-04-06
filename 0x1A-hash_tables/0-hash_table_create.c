080a77607709df3621a269292c656828bbb69e3f
nebihadilsebo <shegolenur@hotmail.com>
2023-04-06 15:55:15 -0700

    hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,17 +10,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = (malloc(sizeof(hash_t))
-			if (ht == NULL)
-			{
-			return (NULL);
-		}
-	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *));
+	hash_table_t *ht;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+	ht = (malloc(sizeof(hash_table_t)));
+	if (ht == NULL)
+		return (NULL);
+	ht->array = malloc(sizeof(hash_table_t *) * size);
 	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	ht->size = size;
 	return (ht);
 }
