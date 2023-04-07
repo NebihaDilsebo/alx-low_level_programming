01ce17613921fdfc1ced74c61f040c7b6b29821b
nebihadilsebo <shegolenur@hotmail.com>
2023-04-07 08:28:09 -0700

    hash_tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		node = ht->array[i];
 		while (node != NULL)
 		{
-			if (flag++)
+			if (!flag)
 				printf(",");
 			printf("'%s': '%s'", node->key, node->value);
 flag = 1;
